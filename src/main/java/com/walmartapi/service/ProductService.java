5496249ea679e145aa2b8813119a0bcca665e5c4
Bastian Prado <bastian.prado.v@gmail.com>
2026-04-08 15:49:47 -0700

    Add entity, repo and inject repo into service; add docker file

--- src/main/java/com/walmartapi/service/ProductService.java
+++ src/main/java/com/walmartapi/service/ProductService.java
@@ -1,16 +1,32 @@
 package com.walmartapi.service;
 
+import com.walmartapi.entity.ProductEntity;
 import com.walmartapi.model.Product;
+import com.walmartapi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
     public Product saveProduct(Product product) {
 
-        // map POJO to entity
-        // Call DB
-        // map entity to POJO
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setDescription(product.getDescription());
+        productEntity.setName(product.getName());
+        productEntity.setPrice(product.getPrice());
+
+        ProductEntity savedEntity = productRepository.save(productEntity);
+
+        product.setName(savedEntity.getName());
+        product.setId(savedEntity.getId());
+        product.setDescription(savedEntity.getDescription());
+        product.setPrice(savedEntity.getPrice());
 
         return product;
     }
