67c616cd8c4995af135575c4c0afc1706360395c
Bastian Prado <bastian.prado.v@gmail.com>
2026-04-09 11:53:55 -0700

    5th commit

--- src/main/java/com/walmartapi/service/ProductService.java
+++ src/main/java/com/walmartapi/service/ProductService.java
@@ -1,34 +1,43 @@
 package com.walmartapi.service;
 
 import com.walmartapi.entity.ProductEntity;
+import com.walmartapi.exception.NotFound;
+import com.walmartapi.mapper.CustomObjectMapper;
 import com.walmartapi.model.Product;
 import com.walmartapi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CustomObjectMapper<ProductEntity, Product> productMapper;
 
-    public ProductService(ProductRepository productRepository){
+    public ProductService(ProductRepository productRepository,
+                          CustomObjectMapper<ProductEntity, Product> productMapper){
         this.productRepository = productRepository;
+        this.productMapper = productMapper;
     }
 
     public Product saveProduct(Product product) {
 
-        ProductEntity productEntity = new ProductEntity();
-        productEntity.setDescription(product.getDescription());
-        productEntity.setName(product.getName());
-        productEntity.setPrice(product.getPrice());
+        ProductEntity newProduct = productMapper.mapToEntity(product);
+        ProductEntity savedEntity = productRepository.save(newProduct);
 
-        ProductEntity savedEntity = productRepository.save(productEntity);
+        return productMapper.mapToDto(savedEntity);
+    }
 
-        product.setName(savedEntity.getName());
-        product.setId(savedEntity.getId());
-        product.setDescription(savedEntity.getDescription());
-        product.setPrice(savedEntity.getPrice());
+    public Product getProductById(Long id){
 
-        return product;
+        Optional<ProductEntity> product = productRepository.findById(id);
+
+        if(product.isEmpty()) {
+            throw new NotFound("Product not found");
+        }
+
+        return productMapper.mapToDto(product.get());
     }
 
 }
